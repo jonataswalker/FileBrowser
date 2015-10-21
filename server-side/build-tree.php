11d102d1bc2cb09694869a1feb9fdcd37ae9e71b
jonataswalker <jonataswalker@gmail.com>
2015-10-21 15:02:09 -0200

    Initial Commit

---
+++ server-side/build-tree.php
@@ -0,0 +1,197 @@
+<?php
+ini_set("display_errors", 1);
+error_reporting(E_ALL ^ E_NOTICE);
+date_default_timezone_set('America/Sao_Paulo');
+
+class buildTreeFromDirectory {
+
+    public  $root_dir,
+            $file_suffix,
+            $qtde_suffix = 3, //small, medium, big
+            $allowed_extensions,
+            $ignore_hidden,
+            $exclude_dir;
+    
+    private $filter;
+
+    public function __construct($root_dir) {
+    
+        $root_dir = realpath($root_dir);
+        if(!$root_dir)
+            die("This directory doesn't exist!");
+    
+        $this->root_dir = $root_dir;
+        $this->setFilter();
+        date_default_timezone_set(@date_default_timezone_get());
+    }
+    public function setFilter(
+            $allowed_extensions = array('*'),
+            $file_suffix = '',
+            $exclude_dir = array(),
+            $ignore_hidden = true
+        ) {
+    
+        $this->allowed_extensions = $allowed_extensions;
+        $this->file_suffix = $file_suffix;
+        $this->ignore_hidden = $ignore_hidden;
+        $this->exclude_dir = $exclude_dir;
+    }
+    public function buildTree($last_created = '') {
+        $dir = new RecursiveDirectoryIterator(
+            $this->root_dir, FilesystemIterator::SKIP_DOTS);
+    
+        $this->filter($dir);
+        $it = new RecursiveIteratorIterator(
+            $this->filter,
+            RecursiveIteratorIterator::SELF_FIRST,
+            RecursiveIteratorIterator::CATCH_GET_CHILD
+        );
+        
+        $tree = array();
+        foreach($it as $fileinfo) {
+            
+            $name = $fileinfo->getFilename();
+            $sub_path_name = $it->getSubPathName();
+            $parts = explode(DIRECTORY_SEPARATOR, $sub_path_name);
+            array_pop($parts);
+            
+            $parentArr = &$tree;
+            
+            //go deep in the file|dir path
+            foreach ($parts as $part) {
+                $parentArr = &$parentArr['dirs'][$part];
+            }
+            
+            if ($fileinfo->isDir()) {
+                //statistics
+                $statistics = $this->countChildren($it->key());
+                $total_files = round($statistics['files'] / $this->qtde_suffix);
+            
+                // Add the final part to the structure
+                if(!empty($last_created) && $it->key() == $last_created) {
+                    $parentArr['dirs'][$name] = array(
+                        'f' => $name,
+                        'c-folders' => $statistics['folders'],
+                        'c-files' => $total_files,
+                        'last' => true
+                    );
+                } else {
+                    $parentArr['dirs'][$name] = array(
+                        'f' => $name,
+                        'c-folders' => $statistics['folders'],
+                        'c-files' => $total_files,
+                        'ffff' => $statistics['files'].'..'.$this->qtde_suffix
+                    );
+                }
+            } else {
+                // Add some file info to the structure
+                if($fileinfo->isLink()) {
+                    $realpath = $fileinfo->getRealPath();
+                    $filesize = filesize($realpath);
+                    $filemtime = filemtime($realpath);
+                } else {
+                    $filesize = $fileinfo->getSize();
+                    $filemtime = $fileinfo->getMTime();
+                }
+                
+                $file_path = (empty($it->getSubPath())) ? '/' : '/'.$it->getSubPath().'/';
+                
+                $parentArr['files'][] = array(
+                    'filename'          => $name,
+                    'filesize'          => $this->fileSizeConvert($filesize),
+                    'date'              => date("d-m-Y H:i", $filemtime),
+                    'relative_path'     => $file_path 
+                );
+            }
+        }
+        unset($parentArr);
+        $this->sortArray($tree);
+        return $tree;
+    }
+    private function sortArray(&$tree) {
+        foreach ($tree as &$value) {
+            if (is_array($value))
+                $this->sortArray($value);
+        }
+        return ksort($tree);
+    }
+    private function countChildren($folder) {
+        $folders = array();
+        $files = array();
+        exec('ls -Rp '.$folder.' | grep "/$" | wc -l', $folders);
+        exec('find '.$folder.' -type f | wc -l', $files);
+        return array(
+            'folders' => $folders[0],
+            'files' => $files[0]
+        );
+    }
+    public function saveJSON($path, $filename) {
+        $tree = $this->buildTree();
+        $path = realpath($path);
+        if($path && is_writable($path)) {
+            $full = $path .DIRECTORY_SEPARATOR. $filename;
+            $write = file_put_contents($full, json_encode($tree));
+            
+            if($write)
+                echo 'Saved: ' . $full . "\n";
+            else
+                echo 'Error trying to save: ' . $full . "\n";
+        }
+    }
+    public function getJSON() {
+        return json_encode(array(
+            'tree' => $this->buildTree()
+        ));
+    }
+    private function filter($dir) {
+    
+        $this->filter = new RecursiveCallbackFilterIterator($dir, 
+            function($current, $key, $iterator) {
+                $filename = $current->getFilename();
+            
+                //ignore all hidden files/directories
+                if($this->ignore_hidden) {
+                    if(substr($filename, 0, 1) == '.')
+                        return false;
+                }
+            
+                // Allow recursion
+                if($iterator->hasChildren() && 
+                    !in_array($filename, $this->exclude_dir)) {
+                    return true;
+                }
+                
+                if($current->isReadable() === false)
+                    return false;
+                
+                    
+                //filter by file extension
+                $path = $current->getPathname();
+                $file_ext = strtolower(pathinfo($path, PATHINFO_EXTENSION));
+                
+                if($this->allowed_extensions[0] == '*') { //no extension filter
+                    $ext_allowed = true;
+                } else {
+                    $ext_allowed = in_array($file_ext, $this->allowed_extensions);
+                }
+                
+                if(!empty($this->file_suffix)) {
+                    //filter by suffix and extension
+                    if(strpos($filename, $this->file_suffix.'.'.$file_ext) !== false && $ext_allowed)
+                        return true;
+                    else
+                        return false;
+                }
+                
+                //filter by extension
+                return $ext_allowed;
+            }
+        );
+    }
+    public function fileSizeConvert($bytes) {
+        $label = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
+        for($i = 0; $bytes >= 1024 && $i < (count($label) -1); $bytes /= 1024, $i++);
+        return round($bytes, 2) . " " . $label[$i];
+    }
+}
+?>
---
+++ server-side/filebrowser.php
@@ -0,0 +1,193 @@
+<?php
+ini_set("display_errors", 1);
+error_reporting(E_ALL ^ E_NOTICE);
+require('./build-tree.php');
+header('Content-Type: application/json');
+
+$allow_ext = array('gif','jpg','png','bmp');
+$root = '/home/writable/tmp';
+$suffix = 'small';
+$suffixes = array('small', 'medium', 'big');
+$tree = new buildTreeFromDirectory($root);
+$tree->setFilter($allow_ext, $suffix);
+
+if($_GET['action'] == 'thumbs'){
+    die($tree->getJSON());
+}
+
+
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+    exit;
+}
+
+if(strtoupper($_SERVER['REQUEST_METHOD']) == 'POST'){
+    $action = $_POST['action'];
+
+    if($action == 'nova-pasta' || $action == 'del-pasta' || $action == 'upload'){
+        $parents = str_replace(',', '/', $_POST['parents']);
+        
+        $root2 = realpath($root.'/'.$parents);
+        
+        if(!is_writable($root2)){
+            die(json_encode(array(
+                'erro' => true,
+                'msg' => 'Sem permissão de escrita!'
+            )));
+        }
+        if(!$root2){
+            die(json_encode(array(
+                'erro' => true,
+                'msg' => 'Erro com o path!'
+            )));
+        }
+    }
+    
+    if($action == 'nova-pasta' || $action == 'del-pasta'){
+
+        $full = $root2.'/'.$_POST['folder'];
+        
+        if($action == 'nova-pasta'){
+            if(is_dir($full)){
+                die(json_encode(array(
+                    'erro' => true,
+                    'msg' => 'Esta pasta já existe!'
+                )));
+            }
+            
+            $mkdir = mkdir($full);
+            if($mkdir){
+                chmod($full, 0777);
+                die(json_encode(array(
+                    'erro' => false,
+                    'tree' => $tree->buildTree($full)
+                )));
+            } else {
+                die(json_encode(array(
+                    'erro' => true,
+                    'msg' => 'Erro ao criar: ' . $parents.'/'.$_POST['folder']
+                )));
+            }
+        } elseif($action == 'del-pasta'){
+            $full = realpath($full);
+            if($full && $full != $root){
+                $rm = array();
+                exec("rm -rf {$full}");
+                exec('echo "$?"', $rm); //0 - no error
+                
+                if($rm[0] == 0){
+                    die(json_encode(array(
+                        'erro' => false,
+                        'tree' => $tree->buildTree($root2)
+                    )));
+                } else {
+                    die(json_encode(array(
+                        'erro' => true,
+                        'msg' => 'Erro ao remover pasta: ' . $parents.'/'.$_POST['folder']
+                    )));
+                }
+            } else {
+                die(json_encode(array(
+                    'erro' => true,
+                    'msg' => 'Erro: ' . $parents.'/'.$_POST['folder']
+                )));
+            }
+        }
+    } elseif($action == 'upload'){
+        $files = $_FILES['file'];
+        if($files && is_array($files['tmp_name'])){
+            //extension
+            $ext = getImageExtension($files['type']['medium']);
+            if($ext){
+                $err_up = false;
+                
+                $filesuffix = $files['name'];
+                foreach($files['tmp_name'] as $k => $tmp){
+                    
+                    $filename = pathinfo($files['name'][$k], PATHINFO_FILENAME);
+                    $filename = safeFilename($filename) .'-'.$k.'.'. $ext;
+                    $full = $root2 .'/'. $filename;
+                    
+                    if(move_uploaded_file($tmp, $full)){
+                        chmod($full, 0777);
+                    } else {
+                        $err_up = true;
+                    }
+                }
+                if($err_up){
+                    echo json_encode(array(
+                        'erro' => true,
+                        'msg'   => 'Erro no upload'
+                    ));
+                } else{
+                    echo json_encode(array(
+                        'erro' => false,
+                        'tree' => $tree->buildTree($root2)
+                    ));
+                }
+            }
+        }
+    } elseif($action == 'del-file'){
+        $arr = explode(',', $_POST['files']);
+        $err = false;
+        
+        $keys = array_keys($arr);
+        $key_last = end($keys);
+        $last = $arr[$key_last];
+        $last_arr = explode('/', $last);
+        array_pop($last_arr);
+        $last_parent = implode('/', $last_arr);
+
+        foreach($arr as $path){
+            $pattern = str_replace($suffix, '*', $path);
+            array_map('unlink', glob($root . $pattern));
+        
+            if(is_file($root . $path)){
+                $err = true;
+            }
+        }
+    
+        if($err){
+            echo json_encode(array(
+                'erro' => true,
+                //'file' => $root . $path,
+                'msg'   => 'Erro na remoção.'
+            ));
+        } else{
+            echo json_encode(array(
+                'erro' => false,
+                //'parent' => $root . $last_parent,
+                'tree' => $tree->buildTree($root . $last_parent)
+            ));
+        }
+    }
+}
+function getImageExtension($mime){
+    switch ($mime) {
+        case 'image/gif':
+            $ext = 'gif';
+            break;
+        case 'image/jpeg':
+            $ext = 'jpg';
+            break;
+        case 'image/png':
+            $ext = 'png';
+            break;
+        case 'image/bmp':
+            $ext = 'bmp';
+            break;
+        default:
+            $ext = false;
+    }
+    return $ext;
+}
+function safeFilename($string, $sep = '-', $extra = null){
+    return strtolower(trim(preg_replace('~[^0-9a-z' . preg_quote($extra, '~') . ']+~i', $sep, unaccent($string)), $sep));
+}
+function unaccent($string){
+    if (strpos($string = htmlentities($string, ENT_QUOTES, 'UTF-8'), '&') !== false){
+        $string = html_entity_decode(preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|tilde|uml);~i', '$1', $string), ENT_QUOTES, 'UTF-8');
+    }
+    return $string;
+}
+
+?>
